3b97282645f0864e565e229bfe08b3aa07836440
Łukasz Król <lionel@o2.pl>
2019-03-06 12:42:22 +0100

    Add title component

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Title.css";
+
+const Title = props => 
+  <div className={style.titleText}>
+    <h1>{props.title}</h1>
+    <p>{props.numberOfTasks}</p>
+  </div>;
+
+export default Title;
