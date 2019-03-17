358759dc01f9147699c08d955af54856e2d48bcf
Łukasz Król <lionel@o2.pl>
2019-03-17 21:38:49 +0100

    Modify Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "app.bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader"},
-          {
-            loader: "css-loader",
-            options: {
-              modules: true
+module.exports = (env) => {
+  const environment = env ||  "production";
+
+  return {
+    mode: environment,
+    entry: "./src/index.js",
+    optimization: {
+      minimize: false,
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "app." + environment + ".bundle.js"
+    }, 
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: "style-loader"},
+            {
+              loader: "css-loader",
+              options: {
+                modules: true
+              }
             }
-          }
-        ]
-      }
-    ]
+          ]
+        }
+      ]
+    }
   }
 };
