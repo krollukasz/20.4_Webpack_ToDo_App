53b8f49bd7b2f2d30fb28db5ccab461e4fe771bb
Łukasz Król <lionel@o2.pl>
2019-04-07 01:03:30 +0200

    Modify component with list item

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,10 @@
 import React from "react";
 import style from "./TodoList.css";
 
-const Todo = props => {
-  return (
-    <li key={props.key}>
-      {props.text}
-      <button className={style.itemRemove} onClick = {() => props.remove(item.id)}>Delete</button>
-    </li>
-  )
-};
+const Todo = props => 
+  <li id={props.id}>
+    {props.text}
+    <button className={style.itemRemove} onClick = {() => props.remove(props.id)}>Delete</button>
+  </li>
 
 export default Todo;
