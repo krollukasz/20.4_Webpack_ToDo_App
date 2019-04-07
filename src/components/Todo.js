4949c5c160ff512ba43d9d65f46917c0b5d796ff
Łukasz Król <lionel@o2.pl>
2019-04-07 21:43:26 +0200

    Add return() for list item

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,13 @@
 import React from "react";
 import style from "./TodoList.css";
 
-const Todo = props => 
-  <li id={props.id}>
-    {props.text}
-    <button className={style.itemRemove} onClick = {() => props.remove(props.id)}>Delete</button>
-  </li>
+const Todo = props => {
+  return (
+    <li id={props.id}>
+      {props.text}
+      <button className={style.itemRemove} onClick = {() => props.remove(props.id)}>Delete</button>
+    </li>
+  )
+}
 
 export default Todo;
